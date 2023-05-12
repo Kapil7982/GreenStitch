a59a2acc39203a47201bbb7cc7554c826409beed
Kapil7982 <rathor798299@gmail.com>
2023-05-12 13:51:15 +0530

    All Done

--- SpringSecurityWithJwt/src/main/java/com/green/controller/LoginController.java
+++ SpringSecurityWithJwt/src/main/java/com/green/controller/LoginController.java
@@ -6,27 +6,47 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.green.model.Customer;
 import com.green.repository.CustomerRepository;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 @RestController
 public class LoginController {
 
 	@Autowired
 	private CustomerRepository customerRepository;
-	
+
 	@GetMapping("/signIn")
-	public ResponseEntity<Customer> getLoggedInCustomerDetailsHandler(Authentication auth){
-		
-		
-		 Customer customer= customerRepository.findByEmail(auth.getName()).orElseThrow(() -> new BadCredentialsException("Invalid Username or password"));
-		 
-		 
-		 return new ResponseEntity<>(customer, HttpStatus.ACCEPTED);
-		
-		
+	public ResponseEntity<Customer> getLoggedInCustomerDetailsHandler(Authentication auth) {
+
+		Customer customer = customerRepository.findByEmail(auth.getName())
+				.orElseThrow(() -> new BadCredentialsException("Invalid Username or password"));
+
+		return new ResponseEntity<>(customer, HttpStatus.ACCEPTED);
+
 	}
-	
+
+	@PostMapping("/signout")
+	public ResponseEntity<Void> signout(HttpServletRequest request, HttpServletResponse response) {
+		// invalidate token
+		Cookie[] cookies = request.getCookies();
+		if (cookies != null) {
+			for (Cookie cookie : cookies) {
+				if (cookie.getName().equals("access_token")) {
+					cookie.setValue("");
+					cookie.setMaxAge(0);
+					cookie.setPath("/");
+					response.addCookie(cookie);
+				}
+			}
+		}
+		return ResponseEntity.ok().build();
+	}
+
 }
